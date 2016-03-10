9132cdf79cdb9d9b49f38649a871ac0417cfdc5c
andrew gibbs <andrewgibbs@outlook.com>
2016-03-10 14:11:58 -0800

    more hw for 4thday

--- hwjs.js
+++ hwjs.js
@@ -86,3 +86,177 @@ while (i < 6) {
 }}
 
 
+
+/************************
+ *  FOR LOOPS
+ ************************/
+
+/* Rewrite the "count up" clock as a for loop! */
+
+for (var i=1; i<6; i++) {
+    console.log(1);
+}
+
+
+
+
+/* Rewrite the for loop to have no initial conditions. */
+
+null;
+for (; null; null) {
+    console.log(null);
+}
+
+/* Rewrite the for loop to have no incrementing statement. */
+
+for (null; null; ) {
+    console.log(null);
+    null;
+}
+
+/* Rewrite the for loop to have no incrementing or intial
+ statements. */
+
+null;
+for (; null; ) {
+    console.log(null);
+    null;
+}
+
+/* Note that this is identical to the while loop! */
+
+
+/* Let's make a count DOWN clock. We will display
+ 5, 4, 3, 2, 1, 0. */
+
+var i = null;
+while (i >= null) {
+    console.log(null);
+    null;
+}
+
+/* Now, rewrite the countdown clock as a for loop! */
+
+for (null; null; null) {
+    console.log(null);
+}
+
+
+/* Using a for loop, print each value of the array
+ to the console. */
+
+var foods = ["Burger", "Fries", "Drink", "Tomato"];
+
+for (i=0; i<null; null) {
+    console.log(null);
+}
+
+/* Using a for loop, print each value of the array
+ to the console, backwards! */
+
+var foods = ["Burger", "Fries", "Drink", "Tomato"];
+
+for (i=foods.length-1; null; null) {
+    console.log(null);
+}
+
+
+
+/************************
+ *  FUNCTIONS
+ ************************/
+
+/* Given the following function, call the function with
+ appropriate example values. In a comment, write
+ what the expected output will be. */
+
+function concatStrings(str, appendString) {
+    return str + appendString;
+}  // returns the concatonate string vlaue
+
+concatStrings("big","hair");  // output is: "big hair"
+
+
+/* Write a function that takes one number as a parameter,
+ and returns that number plus one. Choose good
+ variable names! Call the function with an appropriate
+ parameter. */
+
+function increment(number) {
+    return number +1;
+}
+
+increment(4);  // output is: 5
+
+
+/* Complete the below function. It accepts a
+ number and returns a string with "a" repeated
+ that many times. For example, calling the
+ function with 3 will return "aaa".
+ Use a for loop to construct the return
+ string. Call the function
+ with example values and write the output as
+ a comment. */
+
+function repeatA(numTimesRepeated) {
+    var returnString = "";
+
+    for (null; null; null) {
+        returnString += "a";
+    }
+
+    return returnString;
+}
+
+repeatA(null);   // output is:
+
+
+/* Let's generalize the last function. Now, a
+ second parameter will be passed which is the
+ character (or string) to repeat! Call the function
+ with example values and write the output as
+ a comment. */
+
+function repeatString(numTimesRepeated, stringToRepeat) {
+    // modify the last function's code
+}
+
+repeatString(null, null);  // output is:
+
+
+/* Write a function which returns the original array,
+ but reversed! */
+
+function reverseStrings(arrayOfStrings)
+{
+    var reversedArray = [];
+
+    for (i=arrayOfStrings.length-1; null; null) {
+        reversedArray.push(null);
+    }
+
+    return reversedArray;
+}
+
+var foods = ["Burger", "Fries", "Drink", "Tomato"];
+reverseStrings(foods);
+
+/* Note that variables can be assigned functions. */
+
+var someVariable = repeatA;
+var anotherVariable = repeatA;
+
+/* Try running the above two lines in your browser.
+ Now, try calling the functions by using the
+ new variables. (i.e. run the below code!) */
+
+someVariable(3);
+anotherVariable(5);
+
+/* This is an important fact about JavaScript --
+ functions can be assigned to variables and
+ passed around as arguments to functions! */
+
+
+/* CONGRATULATIONS! YOU MADE IT TO THE END! */
+
